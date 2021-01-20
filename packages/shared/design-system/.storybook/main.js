b46d50efb500d02ae5373e0604e2b3a1deb3c7fb
Ievgenii Loza <ievgenii.loza@xeneta.com>
2021-01-20 18:00:30 +0100

    Part 2: Add additional configurations to `@delipack/design-system`
    - Add `webpack` config to `@storybook/react`
    - Add JS, TS support to `@delipack/design-system`
    - Add `babel` config
    - Add TS configuration and declarations
    - Add `styled-components` support

--- packages/shared/design-system/.storybook/main.js
+++ packages/shared/design-system/.storybook/main.js
@@ -1,4 +1,28 @@
+const merge = require("webpack-merge");
+const path = require("path");
+
 module.exports = {
   stories: ["../src/**/*.stories.tsx"],
-  webpackFinal: async (config) => config,
+  webpackFinal: async (config) =>
+    merge.smartStrategy({
+      "module.rules": "append",
+    })(config, {
+      module: {
+        rules: [
+          {
+            include: path.resolve(path.join(__dirname, "../src")),
+            test: /.*(\.jsx|\.js)$/,
+            use: ["babel-loader"],
+          },
+          {
+            include: path.resolve(path.join(__dirname, "../src")),
+            test: /.*(\.ts|\.tsx)$/,
+            use: ["ts-loader"],
+          },
+        ],
+      },
+      resolve: {
+        extensions: [".ts", ".tsx", ".js", ".jsx"],
+      },
+    }),
 };
