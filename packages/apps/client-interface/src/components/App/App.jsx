63b97c0b4704577d450946646ce258c2a4efb1e9
Ievgenii Loza <ievgenii.loza@xeneta.com>
2021-01-26 16:04:51 +0100

    Part 15: Add `/track/*` page for `@delipack/client-interface`
    - Add TrackPage component
    - Add `/track/*` route

--- packages/apps/client-interface/src/components/App/App.jsx
+++ packages/apps/client-interface/src/components/App/App.jsx
@@ -3,12 +3,16 @@ import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 import { Dashboard } from "../Dashboard";
 import { NotFoundPage } from "../NotFoundPage";
+import { TrackPage } from "../TrackPage";
 
 export const App = () => (
   <Router>
     <Switch>
       <Route exact path="/">
         <Dashboard />
+      </Route>
+      <Route path="/track/:tracking_number">
+        <TrackPage />
       </Route>
       <Route>
         <NotFoundPage />
